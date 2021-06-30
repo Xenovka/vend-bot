ad9c258d5229bed5e7e57b142a03e3da3c740b2f
Xenovka <wendyanto18@gmail.com>
2021-06-30 17:25:30 +0700

    adding queue

--- commands/voiceCommands/playMusic.js
+++ commands/voiceCommands/playMusic.js
@@ -2,9 +2,11 @@ const { MessageEmbed } = require('discord.js')
 const yts = require('yt-search')
 const ytdl = require('ytdl-core')
 
+let queue = new Map()
+
 module.exports = {
   commands: 'play',
-  expectedArgs: '[songTitle]',
+  expectedArgs: '[musicTitle]',
   minArgs: 1,
   maxArgs: 10,
   callback: async (message, arguments, argsText) => {
@@ -12,28 +14,71 @@ module.exports = {
 
     if (!guild) return
 
+    const serverQueue = queue.get(guild.id)
+
     const search = await yts(argsText)
     const searchResult = search.videos[0]
-    const songURL = searchResult.url
-    const songTitle = searchResult.title
+    const musicURL = searchResult.url
+    const musicTitle = searchResult.title
+    const musicDuration = searchResult.timestamp
 
-    if(member.voice.channel) {
-        const connect = await member.voice.channel.join().then(async connection => {
-          const deafen = await connection.voice.setSelfDeaf(true)
-          const dispatcher = connection.play(ytdl(songURL, {filter: 'audioonly'}))
-      })
-      
+    if(member.voice.channel) {        
+        if(!serverQueue) {
+          const musicList = {
+            songs: [],
+            connection: null
+          }
+          
+          queue.set(guild.id, musicList)
+          musicList.songs.push({musicTitle, musicURL, musicDuration})
+
+          try {
+            const connect = await member.voice.channel.join()
+            musicList.connection = connect
+            musicPlayer(channel, guild, musicList.songs[0])
+
+            const embeds = new MessageEmbed()
+              .setColor("ORANGE")
+              .setDescription(`Playing [${musicTitle}](${musicURL}) By <@${author.id}>`)
+
+            channel.send(embeds)
+            
+
+          } catch (err) {
+            return channel.send('There was an error occured while trying to play the song!')
+          }
+        } else {
+          serverQueue.songs.push({musicTitle, musicURL, musicDuration})
+          const embeds = new MessageEmbed()
+              .setColor("ORANGE")
+              .setDescription(`Queued [${musicTitle}](${musicURL}) By <@${author.id}>`)
+
+          return channel.send(embeds)
+        }
+
     } else {
       message.reply(
         "You must be in the voice channel before u tell me to join!"
       );
       return
     }
-
-    const embeds = new MessageEmbed()
-      .setColor("ORANGE")
-      .setDescription(`Playing [${songTitle}](${songURL}) Requested By <@${author.id}>`)
-
-    channel.send(embeds)
   }
+}
+
+const musicPlayer = async (channel, guild, song) =>{ 
+  const musicQueue = queue.get(guild.id)
+  
+  await musicQueue.connection.voice.setSelfDeaf(true) // bot self deafen
+
+  if(!song) {
+    musicQueue.connection.leave()
+    queue.delete(guild.id)
+    return channel.send('Queue is empty now. Bye-bye.')
+  }
+
+  musicQueue.connection.play(ytdl(song.musicURL, {filter: 'audioonly'}))
+    .on('finish', () => {
+      musicQueue.songs.shift()
+      musicPlayer(channel, guild, musicQueue.songs[0])
+    })
 }
