006f54475d647d04acff95fa644b37396fa3b16d
Xenovka <wendyanto18@gmail.com>
2021-07-02 10:59:32 +0700

    refactoring syntax little bit

--- utils/createDefaultPrefix.js
+++ utils/createDefaultPrefix.js
@@ -9,7 +9,7 @@ module.exports = async (client) => {
     try {
       const result = await prefixSchema.findById(guildId);
 
-      if (result === null) {
+      if (!result) {
         await new prefixSchema({
           _id: guildId,
           serverPrefix: "!"
