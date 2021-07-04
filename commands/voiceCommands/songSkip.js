aded307242efc8ee7a65b652665625c1093563c8
Xenovka <wendyanto18@gmail.com>
2021-07-04 09:41:08 +0700

    song skip done

--- commands/voiceCommands/songSkip.js
+++ commands/voiceCommands/songSkip.js
@@ -6,19 +6,33 @@ module.exports = {
   callback: async (message, arguments, argsText) => {
     const {channel, member} = message
 
-    await mongodb().then(async mongoose => {
-      try {
-        const songs = await songSchema.findOne({nowPlaying: true})
-
-        if(!songs) {
-          return channel.send('Empty Queue!')
+    if(member.voice.channel) {
+      await mongodb().then(async mongoose => {
+        try {
+          const songs = await songSchema.findOneAndUpdate({nowPlaying: true}, {$set: {nowPlaying: false}})
+          const index = songs.queueNumber;
+  
+          if(!index) {
+            return channel.send('Already at the end of the queue!')
+          }
+  
+          if(!songs) {
+            return channel.send('Empty Queue!')
+          }
+  
+          await songSchema.findOneAndUpdate({queueNumber: index + 1}, {$set: {nowPlaying: true}}) // Set the next song to be played
+  
+        } finally {
+          mongoose.connection.close()
         }
+      })
+    } else {
+      message.reply(
+        "You must be in the voice channel to use this command!"
+      );
+      return
+    }
 
-        console.log(songs)
-
-      } finally {
-        mongoose.connection.close()
-      }
-    })
+    
   }
 }
