58f3dc4a4397b83e7dd96c8e5bb4a5f357f43406
Xenovka <wendyanto18@gmail.com>
2021-07-06 10:27:42 +0700

    add server latency information

--- commands/chatCommands/ping.js
+++ commands/chatCommands/ping.js
@@ -1,10 +1,10 @@
 module.exports = {
   commands: 'ping',
-  callback: (message, arguments, argsText) => {
+  callback: (message, arguments, argsText, client) => {
     message.channel.send('Loading. . .').then(result => {
       const ping = result.createdTimestamp - message.createdTimestamp
 
-      result.edit(`Bot Latency: ${ping}ms`);
+      result.edit(`Bot Latency: ${ping}ms\nServer Latency: ${client.ws.ping}ms`);
     })
   }
 }
