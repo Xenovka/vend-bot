3932effeaf3499f3eacbb1bd7141eaa1053640dd
Xenovka <wendyanto18@gmail.com>
2021-07-06 08:30:26 +0700

    kick command

---
+++ commands/modCommands/kickMember.js
@@ -0,0 +1,30 @@
+module.exports = {
+  commands: 'kick',
+  expectedArgs: '[@user] [reasons]',
+  permissionError:
+  'Only member with permission of "ADMINISTRATOR" can use this command.',
+  minArgs: 1,
+  maxArgs: 30,
+  permissions: 'ADMINISTRATOR',
+  callback: async (message, arguments, argsText) => {
+    const {guild, mentions, channel} = message
+
+    const mentionedUser = mentions.users.first()
+    const kickReason = arguments.slice(1, arguments.length).join(' ')
+
+    if (!mentionedUser) {
+      channel.send('You should mention the user you want to kick from the guild.')
+      return;
+    }
+
+    if(!kickReason) {
+      await guild.member(mentionedUser.id).kick()
+      channel.send(`<@${mentionedUser.id}> has been kicked for the server for no reason.`)
+      return
+    }
+    
+    await guild.member(mentionedUser.id).kick(kickReason)
+    channel.send(`<@${mentionedUser.id}> has been kicked for the server because ${kickReason}.`)
+
+  }
+}
