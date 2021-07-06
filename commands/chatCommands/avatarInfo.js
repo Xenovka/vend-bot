be8f42d486205b150bfd0130e364b3e82f57259e
Xenovka <wendyanto18@gmail.com>
2021-07-06 10:40:34 +0700

    showing other member's avatar

--- commands/chatCommands/avatarInfo.js
+++ commands/chatCommands/avatarInfo.js
@@ -2,16 +2,32 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   commands: 'avatar',
+  minArgs: 0,
+  maxArgs: 1,
   callback: (message, arguments, argsText) => {
-    const {author, channel} = message
+    const {author, channel, mentions, guild} = message
 
-    const embed = new MessageEmbed()
-      .setAuthor(`${author.username}'s avatar`)
-      .setImage(author.avatarURL())
+    const mentionedUser = mentions.users.first()
+
+    if(!mentionedUser) {
+      const embeds = new MessageEmbed()
+        .setAuthor(`${author.username}'s avatar`)
+        .setImage(author.avatarURL())
+        .setFooter("Your avatar looks soo . . . .")
+        .setTimestamp();
+
+      channel.send(embeds);
+      return
+    }
+
+    const embeds = new MessageEmbed()
+      .setAuthor(`${mentionedUser.username}'s avatar`)
+      .setImage(mentionedUser.avatarURL())
       .setFooter("Your avatar looks soo . . . .")
       .setTimestamp();
 
-    channel.send(embed);
+    channel.send(embeds);
+    
   }
 
 }
