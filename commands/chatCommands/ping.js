dab70ebb216d0fd84fa6566b3b61a0615a405c0c
Xenovka <wendyanto18@gmail.com>
2021-07-06 10:19:28 +0700

    changed a bit

--- commands/chatCommands/ping.js
+++ commands/chatCommands/ping.js
@@ -1,6 +1,6 @@
 module.exports = {
   commands: 'ping',
-  callback: (message, arguments, argsText, client) => {
+  callback: (message, arguments, argsText) => {
     message.channel.send('Loading. . .').then(result => {
       const ping = result.createdTimestamp - message.createdTimestamp
 
