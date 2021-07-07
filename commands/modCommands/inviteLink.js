5028938e678b3ee0d430cd0df9d594fc9092f86b
Xenovka <wendyanto18@gmail.com>
2021-07-07 07:51:00 +0700

    delete unnecessary comments

--- commands/modCommands/inviteLink.js
+++ commands/modCommands/inviteLink.js
@@ -1,5 +1,3 @@
-// const commands = require("../commandHandler");
-
 module.exports = {
   commands: 'invite',
   permissionError:
@@ -23,23 +21,3 @@ module.exports = {
     });
   }
 }
-
-// module.exports = (client) => {
-//   commands(client, "invite", async (message) => {
-//     const { guild, channel } = message;
-
-//     const createInviteLink = await guild.fetchInvites().then((invites) => {
-//       let results = [];
-
-//       invites.forEach((invite) => {
-//         results.push(invite.code);
-//       });
-
-//       const createdInviteLink = `https://discord.gg/${
-//         results[results.length - 1]
-//       }`;
-
-//       channel.send(createdInviteLink);
-//     });
-//   });
-// };
