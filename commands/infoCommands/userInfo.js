674f88eeb6cdce9594b0a03378af43c0ed060112
Xenovka <wendyanto18@gmail.com>
2021-07-11 12:07:03 +0700

    refactoring codes

--- commands/infoCommands/userInfo.js
+++ commands/infoCommands/userInfo.js
@@ -4,62 +4,72 @@ const moment = require("moment");
 module.exports = {
   commands: "info",
   cooldown: 5,
+  minArgs: 0,
+  maxArgs: 1,
   callback: ({ message }) => {
-    const { author, channel, member } = message;
+    const { author, channel, member, mentions } = message;
 
-    const userTag = author.tag;
-    const userId = author.id;
-    const userAvatar = author.avatarURL();
-    const nickname = "None" || member.nickname;
-    const userCreatedAt = moment(author.createdAt).format(
-      "dddd, D MMMM YYYY, hh:mm:ss A"
-    );
-    const userJoinDate = moment(member.joinedAt).format(
-      "dddd, D MMMM YYYY, hh:mm:ss A"
-    );
-    let userRoles = [];
+    const mentionedMember = mentions.members.first();
 
-    member.roles.cache
-      .filter((key) => key.name !== "@everyone")
-      .each((key) => userRoles.push(`<@&${key.id}>`));
-
-    const embeds = new MessageEmbed()
-      .setAuthor(userTag, userAvatar)
-      .setThumbnail(userAvatar)
-      .setColor("ORANGE")
-      .addFields(
-        {
-          name: "ID",
-          value: userId,
-          inline: true
-        },
-        { name: "_ _", value: "_ _", inline: true },
-        {
-          name: "Nickname",
-          value: nickname,
-          inline: true
-        },
-        {
-          name: "Account Created",
-          value:
-            userCreatedAt + " [`" + moment(author.createdAt).fromNow() + "`]",
-          inline: false
-        },
-        {
-          name: "Join Date",
-          value:
-            userJoinDate + " [`" + moment(member.joinedAt).fromNow() + "`]",
-          inline: false
-        },
-        {
-          name: "Roles",
-          value: userRoles.join(", "),
-          inline: false
-        }
-      )
-      .setFooter("Showing your informations")
-      .setTimestamp();
-
-    channel.send(embeds);
+    if (!mentionedMember) {
+      embedMessage(author, member, channel);
+    } else {
+      embedMessage(mentionedMember.user, mentionedMember, channel);
+    }
   }
+};
+
+const embedMessage = (user, member, channel) => {
+  const userTag = user.tag;
+  const userId = user.id;
+  const userAvatar = user.avatarURL();
+  const nickname = member.nickname || "None";
+  const userCreatedAt = moment(user.createdAt).format(
+    "dddd, D MMMM YYYY, hh:mm:ss A"
+  );
+  const userJoinDate = moment(member.joinedAt).format(
+    "dddd, D MMMM YYYY, hh:mm:ss A"
+  );
+  let userRoles = [];
+
+  member.roles.cache
+    .filter((key) => key.name !== "@everyone")
+    .each((key) => userRoles.push(`<@&${key.id}>`));
+
+  const embeds = new MessageEmbed()
+    .setAuthor(userTag, userAvatar)
+    .setThumbnail(userAvatar)
+    .setColor("ORANGE")
+    .addFields(
+      {
+        name: "ID",
+        value: userId,
+        inline: true
+      },
+      { name: "_ _", value: "_ _", inline: true },
+      {
+        name: "Nickname",
+        value: nickname,
+        inline: true
+      },
+      {
+        name: "Account Created",
+        value: userCreatedAt + " [`" + moment(user.createdAt).fromNow() + "`]",
+        inline: false
+      },
+      {
+        name: "Join Date",
+        value: userJoinDate + " [`" + moment(member.joinedAt).fromNow() + "`]",
+        inline: false
+      },
+      {
+        name: "Roles",
+        value: userRoles.join(", "),
+        inline: false
+      }
+    )
+    .setFooter(`Showing Informations of ${user.username}`)
+    .setTimestamp();
+
+  return channel.send(embeds);
 };
