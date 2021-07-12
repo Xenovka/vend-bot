67c3fa8fd6b6dd41904bf16f5be36c3f8ae448d6
Xenovka <wendyanto18@gmail.com>
2021-07-12 10:32:39 +0700

    add daily command

--- commands/infoCommands/helpCommands.js
+++ commands/infoCommands/helpCommands.js
@@ -43,7 +43,7 @@ module.exports = {
         {
           name: "Economy  💸",
           value:
-            "[`balance`](https://github.com/Xenovka?tab=repositories 'Check your balance.')",
+            "[`balance`](https://github.com/Xenovka?tab=repositories 'Check your balance.') [`daily`](https://github.com/Xenovka?tab=repositories 'Claim daily rewards.')",
           inline: true
         },
         {
