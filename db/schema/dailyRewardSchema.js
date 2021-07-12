d8e4c4b88f1fb3161cf52e68f719a726277dc272
Xenovka <wendyanto18@gmail.com>
2021-07-12 09:42:42 +0700

    schema for claiming daily rewards

---
+++ db/schema/dailyRewardSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const reqString = {
+  type: String,
+  required: true
+};
+
+const dailyRewardSchema = new mongoose.Schema(
+  {
+    guildId: reqString,
+    userId: reqString
+  },
+  {
+    timestamp: true
+  }
+);
+
+module.exports = mongoose.Model("daily-reward", dailyRewardSchema);
